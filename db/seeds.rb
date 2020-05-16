ccc95a7bf9542326958da59fe1392159fe628685
William Goh <wirriamm@gmail.com>
2020-05-16 16:33:00 +0800

    added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Seeding flats'
+5.times do |i|
+  puts "Seeding Flat number #{i}"
+  Flat.create!(
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  )
+end
+puts 'Done seeding'
