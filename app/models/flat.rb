6209a002d47e3e902bbd97ca8381d5d8b4ac83ea
William Goh <wirriamm@gmail.com>
2020-05-16 16:06:57 +0800

    Flat validations

---
+++ app/models/flat.rb
@@ -0,0 +1,6 @@
+class Flat < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :price_per_night, presence: true, numericality: { only_integer: true }
+  validates :number_of_guests, presence: true, numericality: { only_integer: true }
+end
