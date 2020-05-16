0b13b8be3edbe3c9999a29ba6feccd58dbfcaf5b
William Goh <wirriamm@gmail.com>
2020-05-16 20:53:07 +0800

    included new

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,15 +1,21 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, only: [:show]
   def index
     @flats = Flat.all
   end
 
-  def show
+  def
+    raise
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    flat = Flat.new(flat_params)
+    flat.save
+    redirect_to flat_path(flat)
   end
 
   def edit
@@ -19,5 +25,15 @@ class FlatsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :number_of_guests, :price_per_night)
   end
 end
